4e42955e0ccfde23261d7e22dfbec4ca6148eaf6
gibeulah <gibeulah@microsoft.com>
2018-09-04 00:10:31 +0530

    First Push

---
+++ DepthFirstSearchUsingAdjacencyMatrix/Graph.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DepthFirstSearchUsingAdjacencyMatrix
+{
+    class Graph
+    {
+        private int Vertices;
+        private int[,] AdjacencyMatrix;
+
+        private Graph() { }
+
+        public Graph(int Vertices, int[,] adjMatrix) {
+            this.Vertices = Vertices;
+            this.AdjacencyMatrix = adjMatrix;
+        }
+
+        public void DFS(int startVertex) {
+            Boolean []visisted = new Boolean[this.Vertices];
+            Console.WriteLine("The Depth First Search is as follows");
+            DFSUtil(startVertex, this.AdjacencyMatrix, visisted);
+            Console.ReadKey();
+        }
+
+
+        private void DFSUtil(int startVertex, int[,] matrix, Boolean[] visited) {
+
+            visited[startVertex] = true;
+            Console.Write(startVertex + "->");
+            for (int i = 0; i < Vertices; i++) {
+                if (AdjacencyMatrix[i, startVertex] == 1 && false == visited[i]) {
+                    DFSUtil(i, matrix, visited);
+                }
+            }
+        }
+
+
+        public void DisplayAdjacencyMatrix() {
+            for (int i = 0; i < Vertices; i++) {
+                for (int j = 0; j < Vertices; j++) {
+                    Console.Write(AdjacencyMatrix[i,j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
--- DepthFirstSearchUsingAdjacencyMatrix/Program.cs
+++ DepthFirstSearchUsingAdjacencyMatrix/Program.cs
@@ -6,7 +6,25 @@ namespace DepthFirstSearchUsingAdjacencyMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Depth First Search using Adjacency Matrix!");
+            Console.WriteLine("------------------------------------------");
+
+            Console.WriteLine("Enter the number of vertices");
+            int noOfVertices = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the elements [" + noOfVertices + "*" + noOfVertices + "=" + (noOfVertices * noOfVertices) + "]");
+            int [,]adjMatrix = new int[noOfVertices, noOfVertices];
+            for (int i = 0; i < noOfVertices; i++) {
+                for (int j = 0; j < noOfVertices; j++) {
+                    adjMatrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Graph g = new Graph(noOfVertices, adjMatrix);
+            Console.WriteLine("The adjacency Matrix is");
+            g.DisplayAdjacencyMatrix();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Enter the start index");
+            int startIndex = int.Parse(Console.ReadLine());
+            g.DFS(startIndex);
         }
     }
 }
